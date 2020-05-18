f2537b55ddccd1cd8668875dba4ee1ebb5bcf72d
Adam Makler <aumakler@gmail.com>
2020-05-18 22:15:23 +0200

    pierwszy

---
+++ main.js
@@ -0,0 +1,10 @@
+<!-- alert ('siema'); -->
+
+const firstName = 'Adam';
+const age = 45;
+
+
+console.log(firstName);
+console.log(age);
+
+console.log(`Siema nazywam sie ${firstName}, mam ${age} lat.`)
